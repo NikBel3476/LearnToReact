bb038cb0f32b8264e20b784a8b34081b0e9bd98e
NikBel3476 <63227044+NikBel3476@users.noreply.github.com>
2020-08-23 17:46:51 +0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,103 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './App.css';
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walkie',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramovm Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list,
+    };
+
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  onDismiss(id) {
+    const isNotId = item => item.objectID !== id;
+    const updatedList = this.state.list.filter(isNotId);
+    this.setState({ list: updatedList });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.list.map(item => {
+          const onHandleDismiss = () => 
+            this.onDismiss(item.objectID);
+
+          return (
+           <div key={item.objectID}>
+             <span>
+               <a href={item.url}>{item.title}</a>
+             </span>
+             <span>{item.author}</span>
+             <span>{item.num_comments}</span>
+             <span>{item.points}</span>
+             <span>
+                <button 
+                  onClick={() => console.log(item.objectID)}
+                  type="button"
+                >
+                  Отбросить
+                </button>
+             </span>
+           </div>
+          ) 
+        }
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
+
+/*class ExplainBindingsComponent extends Component {
+  constructor() {
+    super();
+
+    this.onClickMe = this.onClickMe.bind(this);
+  }
+
+  onClickMe() {
+    console.log(this);
+  }
+
+  render() {
+    return (
+      <button
+        onClick={this.onClickMe}
+        type="button"
+      >
+        Нажми на меня
+      </button>
+    );
+  }
+}*/
+
+//export default ExplainBindingsComponent;
+
+
+/*import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -23,4 +122,23 @@ function App() {
   );
 }
 
-export default App;
+export default App;*/
+
+/*class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        {list.map(item => 
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </div>
+        )}
+      </div>
+    );
+  }
+}*/
